acf4f66e1506e15dc90a7706ca2ce310cb9355fc
genie-ru <genie.zgmf987@gmail.com>
2025-02-16 19:42:20 +0900

    add test001

--- test001/src/main.rs
+++ test001/src/main.rs
@@ -1,3 +1,32 @@
+use std::str::FromStr;
+use std::env;
+
+/// 最大公約数 (GCD) を求める関数
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+
+    if numbers.is_empty() {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
